02bffc4b3ae47617cf0e054354f13ee15ddfd86d
Oleg Babin <babinoleg@mail.ru>
2022-01-10 22:06:59 +0300

    disable vshard.storage in case of operation error
    In case of OperationError (config was unsuccessfully applied on
    storage) we shouldn't perform request to such storage.
    After this feature was implemented in vshard
    (https://github.com/tarantool/vshard/issues/298) we could just
    disable vshard storage on such instances. For this purpose simple
    trigger on_apply_config was implemented.
    
    Closes #1411

--- cartridge/roles.lua
+++ cartridge/roles.lua
@@ -509,6 +509,30 @@ local function reload()
     return confapplier.apply_config(clusterwide_config)
 end
 
+local function on_apply_config(conf, state)
+    checks('table', 'string')
+    if conf.__type == 'ClusterwideConfig' then
+        local err = "Bad argument #1 to apply_config" ..
+            " (table expected, got ClusterwideConfig)"
+        error(err, 2)
+    end
+
+    local my_replicaset = conf.topology.replicasets[box.info.cluster.uuid]
+    local enabled_roles = get_enabled_roles(my_replicaset.roles)
+    for _, role in ipairs(vars.roles_by_number) do
+        if enabled_roles[role.role_name] then
+            if type(role.M.on_apply_config) == 'function' then
+                local _, err = ApplyConfigError:pcall(
+                    role.M.on_apply_config, conf, state
+                )
+                log.error('%s', err)
+            end
+        end
+    end
+
+    return true
+end
+
 return {
     cfg = cfg,
     get_role = get_role,
@@ -519,6 +543,7 @@ return {
 
     validate_config = validate_config,
     apply_config = apply_config,
+    on_apply_config = on_apply_config,
     reload = reload,
     stop = stop,
 }
